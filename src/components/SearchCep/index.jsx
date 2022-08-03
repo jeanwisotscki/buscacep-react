a33d8a71fdd77b3d6dd6b672caadcf1b58df9c61
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-08-03 11:19:05 -0300

    input

--- src/components/SearchCep/index.jsx
+++ src/components/SearchCep/index.jsx
@@ -4,9 +4,13 @@ import styles from "./index.module.css";
 
 export const SearchCep = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <label className={styles.label}>Digite um CEP:</label>
-      <input className={styles.input} type="text" />
+      <input
+        className={styles.input}
+        type="number"
+        placeholder="Somente números"
+      />
     </div>
   );
 };
