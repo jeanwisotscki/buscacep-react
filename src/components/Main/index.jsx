76b7eca2a49acda04e20032d123191883eb45987
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-08-05 11:16:49 -0300

    ajustes layout

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,5 +3,5 @@ import React from "react";
 import styles from "./index.module.css";
 
 export const Main = ({ children }) => {
-  return <div className={styles.main}> {children} </div>;
+  return <main className={styles.main}> {children} </main>;
 };
